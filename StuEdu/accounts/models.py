bc1a647e927937a0abde46645ec92e208396c826
Vijeth-poojary <poojaryvijeth08@gmail.com>
2025-09-17 15:32:43 +0530

    First commit

---
+++ StuEdu/accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+        ('parent', 'Parent'),
+
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    
